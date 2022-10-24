119be88e40d1baacf29123adaec772808a0ff745
Patrycja Mysiak <mysiak.patrycja@gmail.com>
2022-10-24 22:59:39 +0200

    filters styled

--- src/Components/Filters.js
+++ src/Components/Filters.js
@@ -10,22 +10,28 @@ export default function Filters({filterData}) {
     }, [title, tournament])
 
     return(
-        <div>
-            <select 
-                onChange = {e => {
-                    setTitle(e.currentTarget.value)
-                }} 
-            >
-                <option value="">-</option>
-                <option value="CSGO">CSGO</option>
-                <option value="LOL">LOL</option>
-                <option value="DOTA">DOTA</option>
-            </select>
-            <input 
-                onChange = {e => {
-                    setTournament(e.currentTarget.value)
-                }}
-                type="text" />
+        <div className='filters-container'>
+            <div className="select-filter">
+            <h3>Filter by Title: </h3>
+                <select 
+                    onChange = {e => {
+                        setTitle(e.currentTarget.value)
+                    }} 
+                >
+                    <option value="">-</option>
+                    <option value="CSGO">CSGO</option>
+                    <option value="LOL">LOL</option>
+                    <option value="DOTA">DOTA</option>
+                </select>
+            </div>
+            <div className="input-filter">
+            <h3>Filter by Tournament:</h3>
+                <input 
+                    onChange = {e => {
+                        setTournament(e.currentTarget.value)
+                    }}
+                    type="text" />
+            </div>
         </div>
     )
 }
